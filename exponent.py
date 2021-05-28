99adbb421f581ebff28a21d3710f6fed3f006ed2
noat <84829615+noatmc@users.noreply.github.com>
2021-05-28 15:15:43 +0700

    we do a little trolling

---
+++ exponent.py
@@ -0,0 +1,18 @@
+import time
+
+a = int(input("hello give exponent number -> "))
+b = int(input("hello give number again -> "))
+
+# math codes bull shit right here :)
+
+c = a
+k = -1
+
+# main code bullshit for mathematics purpose
+while c > 0:
+  print(c)
+  d = c / b
+  k = k + 1
+  c = d
+if c == 0:
+  print("answer -> ", k)
